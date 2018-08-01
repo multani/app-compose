4eaa7c86cff39dd8147fa52595f84b1b9c337046
Jonathan Ballet <jon@multani.info>
2018-08-02 00:26:26 +0200

    wip write PIDs of execute programs to a local dir

--- app_compose.py
+++ app_compose.py
@@ -3,24 +3,15 @@
 import asyncio
 import itertools
 import os
+import os.path
+import shlex
+import types
 
 import click
 import colorama
 import yaml
 import related
 
-
-@related.immutable
-class Service:
-    name        = related.StringField()
-    command     = related.StringField(required=True)
-    cwd         = related.StringField(default=os.getcwd())
-    environment = related.ChildField(dict, default={})
-
-
-@related.immutable
-class Services:
-    services = related.MappingField(Service, "name")
 
 
 class Color:
@@ -91,44 +82,92 @@ class ProcessOutputProtocol(asyncio.SubprocessProtocol):
 
 
 @click.group()
-def cli():
+@click.option("-f", "--file", default="app-compose.yml", type=click.Path(exists=True))
+@click.pass_context
+def cli(ctx, file):
     colorama.init()
+    ctx.obj.config_file = file
 
 
 @cli.command()
-def up():
-    with open('app-compose.yml') as fp:
-        config = related.from_yaml(fp, Services)
-
+@click.pass_context
+def up(ctx):
     loop = asyncio.get_event_loop()
-    palette = Palette()
-
-    tasks = []
-    for i, (name, service) in enumerate(config.services.items()):
-        app = create_service(loop, service, palette.next_nice())
-        tasks.append(app)
-
-    loop.run_until_complete(asyncio.gather(*tasks))
+    composer = Composer(ctx.obj.config_file, loop=loop)
+    loop.run_until_complete(composer.run())
 
 
-async def create_service(loop, service, color):
-    exit_event = asyncio.Event()
-    transport, protocol = await loop.subprocess_shell(
-        lambda: ProcessOutputProtocol(service.name, color, exit_event),
-        cmd=service.command,
-        cwd=service.cwd,
-        stdin=None,
-        env=service.environment,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.STDOUT,
-    )
+def makedirs(path):
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
 
-    await exit_event.wait()
-    transport.close()
+
+class Composer:
+    def __init__(self, config_file, *, loop):
+        self.loop = loop
+        self.config_file = config_file
+
+        self.project_dir = os.path.dirname(os.path.realpath(config_file))
+
+        self.work_dir = os.path.join(self.project_dir, ".app-compose")
+        self.pids_dir = os.path.join(self.work_dir, "pids")
+
+        makedirs(self.work_dir)
+        makedirs(self.pids_dir)
+
+    def read_config(self):
+
+        @related.immutable
+        class Service:
+            name        = related.StringField()
+            command     = related.StringField(required=True)
+            cwd         = related.StringField(default=self.project_dir)
+            environment = related.ChildField(dict, default={})
+
+        @related.immutable
+        class Services:
+            services = related.MappingField(Service, "name")
+
+        with open(self.config_file) as fp:
+            config = related.from_yaml(fp, Services)
+
+        return config
+
+    def run(self):
+        config = self.read_config()
+        palette = Palette()
+
+        tasks = []
+        for i, (name, service) in enumerate(config.services.items()):
+            app = self.create_service(service, palette.next_nice())
+            tasks.append(app)
+
+        return asyncio.gather(*tasks)
+
+    async def create_service(self, service, color):
+        exit_event = asyncio.Event()
+        args = shlex.split(service.command)
+        transport, protocol = await self.loop.subprocess_exec(
+            lambda: ProcessOutputProtocol(service.name, color, exit_event),
+            *args,
+            cwd=service.cwd,
+            stdin=None,
+            env=service.environment,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.STDOUT,
+        )
+
+        with open(os.path.join(self.pids_dir, service.name), "w") as fp:
+            fp.write(str(transport.get_pid()))
+
+        await exit_event.wait()
+        transport.close()
 
 
 def main():
-    cli(auto_envvar_prefix='AC')
+    cli(auto_envvar_prefix='AC', obj=types.SimpleNamespace())
 
 
 if __name__ == '__main__':
